0d1005cf5dfcc6f1f67d18277ef075e45962593b
jacobkap <jkkaplan6@gmail.com>
2018-06-10 11:39:35 -0400

    init

---
+++ utils.js
@@ -0,0 +1,13 @@
+function getData() {
+  var data = $.getJSON({
+    url: agency_type + ".json",
+    type: 'get',
+    dataType: 'json',
+    async: false,
+    success: function(data) {
+      result = data;
+    }
+  });
+  data = data.responseJSON;
+  return (data);
+}
