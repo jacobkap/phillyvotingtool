f0696c7d6150aadd8db655b6ef21eaafa783f389
jacobkap <jacobkap@sas.upenn.edu>
2019-06-22 08:38:35 -0400

    add google analytics

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
 elections = [
+  "2019 Primary",
   "2018 General",
   "2018 Primary",
   "2017 General",
@@ -20,6 +21,7 @@ elections = [
 ];
 
 elections_no_special = [
+  "2019 Primary",
   "2018 General",
   "2018 Primary",
   "2017 General",
